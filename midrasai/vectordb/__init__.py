e3e7d32e0a07fab38892f4d29c19c7d9875c0496
Thana Paris <thanamparis@gmail.com>
2025-04-13 14:53:51 -0400

    adjusting package export

--- midrasai/vectordb/__init__.py
+++ midrasai/vectordb/__init__.py
@@ -8,3 +8,11 @@ if importlib.util.find_spec("qdrant_client"):
     _, __ = Qdrant, AsyncQdrant  # Redundant alias for F401
 
     __all__.extend(["Qdrant", "AsyncQdrant"])
+
+# Check for AstraDB support
+if importlib.util.find_spec("astrapy"):
+    from midrasai.vectordb._astradb import AstraDB
+
+    _ = AstraDB  # Redundant alias for F401
+
+    __all__.extend(["AstraDB"])
